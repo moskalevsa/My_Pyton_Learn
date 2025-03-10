b9118abf4d8d784b68367163b0e0c3b104f87cb4
moskalevsa <moskalevsa@yandex.ru>
2025-03-10 13:33:10 +0300

    изменен формат вывода

--- git_config_ДЗ_Pro.py
+++ git_config_ДЗ_Pro.py
@@ -1,5 +1,5 @@
 # Импортируем библиотеку
-import subprocess
+from subprocess import run
 
 # Определяем функцию git_config_list, которая будет выполнять команду Git 
 # (нужно в консоль вывести результат работы команды git: git config --global --list)
@@ -7,9 +7,9 @@ def git_config_list():
     # Удаляем заглушку, создаем переменную result:
     # Используем subprocess.run для выполнения команды в переменной result
 
-    result = subprocess.run('git config --global --list', shell = True,)
+    result = run('git config --global --list', shell = True, )
      # Выводим результат выполнения команды result.stdout
-    print (result.stdout)
+    return print (f'{result.stdout}')
 
 # вызываем git_config_list()
-git_config_list()
+print(git_config_list())
